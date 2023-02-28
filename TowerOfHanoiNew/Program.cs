c3eb33e6645dc563e7a044cc3e788b3df9bbecf5
Sean Hyde <seanhyde@Seans-MacBook-Air.local>
2023-03-01 08:28:06 +1000

    Tower of Hanoi

---
+++ TowerOfHanoiNew/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TowerOfHanoiNew
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            int numberOfDisks;
+            Console.Write("Number of disks = ");
+            numberOfDisks = Convert.ToInt32(Console.ReadLine());
+            char startRod = 'A';
+            char tempRod = 'B';
+            char endRod = 'C';
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            TowerOfHanoiNew(numberOfDisks, startRod, tempRod, endRod);
+            
+            sw.Stop();
+            Console.WriteLine("Computation time = " + sw.ElapsedMilliseconds + " milliseconds");
+        }
+
+        private static void TowerOfHanoiNew(int n, char startRod, char tempRod, char endRod)
+        {
+            if (n > 0)
+            {
+                TowerOfHanoiNew(n - 1, startRod, endRod, tempRod);
+                Console.WriteLine("Move top disk from " + startRod + ' ' + endRod);
+                TowerOfHanoiNew(n - 1, tempRod, startRod, endRod);
+            }
+        }
+    }
+}
